7ab5f6c1f4a2f0660ab089219c1d358671c36789
wraggs <wraggstest123@gmail.com>
2018-03-17 00:24:48 +0800

    Initial commit

---
+++ src/tut_153.java
@@ -0,0 +1,9 @@
+public class tut_153
+{
+    public static void main(String args[])
+        {
+            String s = "Strings are immutable";
+            char result =  s.charAt(17);
+            System.out.println(result);
+        }
+}
